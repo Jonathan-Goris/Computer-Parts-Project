51d093b9153ef44a1e4974abb19471759e452b15
Jonathan-Goris <52085966+Jonathan-Goris@users.noreply.github.com>
2019-06-26 19:55:29 -0400

    Add files via upload

---
+++ Splash Screen/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Splash_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.CreateNewAccButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(156, 139);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(156, 197);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Location = new System.Drawing.Point(156, 248);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(100, 28);
+            this.SignInButton.TabIndex = 4;
+            this.SignInButton.Text = "Sign In";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // CreateNewAccButton
+            // 
+            this.CreateNewAccButton.Location = new System.Drawing.Point(633, 139);
+            this.CreateNewAccButton.Name = "CreateNewAccButton";
+            this.CreateNewAccButton.Size = new System.Drawing.Size(134, 28);
+            this.CreateNewAccButton.TabIndex = 5;
+            this.CreateNewAccButton.Text = "Create New Account";
+            this.CreateNewAccButton.UseVisualStyleBackColor = true;
+            this.CreateNewAccButton.Click += new System.EventHandler(this.CreateNewAccButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CreateNewAccButton);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button SignInButton;
+        private System.Windows.Forms.Button CreateNewAccButton;
+    }
+}
+
---
+++ Splash Screen/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Splash_Screen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            SqlCommand CMD = new SqlCommand;
+
+            CMD.CommandText = "Select * From Where Username = @Uname AND Password = @Pword";
+            CMD.Parameters.Add("@Uname", SqlDbType.VarChar, 50, "Username");
+            CMD.Parameters["@Uname"].Value = UsernameTextBox.Text;
+            CMD.Parameters.Add("@Pword", SqlDbType.VarChar, 50, "Password");
+            CMD.Parameters["@Pword"].Value = UsernameTextBox.Text;
+
+            if (/*Enter condtion for successful sign in*/)
+            {
+                // Enter product form here
+            }
+            else
+            {
+                MessageBox.Show("Username or Password are incorrect");
+            }
+        }
+
+        private void CreateNewAccButton_Click(object sender, EventArgs e)
+        {
+            //Enter Create New Account form here
+        }
+    }
+}
